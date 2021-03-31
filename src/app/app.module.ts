444f7601a7ad75c26168d894425f5446855070f6
Artrogeno <root.arthur@gmail.com>
2021-03-30 18:56:50 -0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
 
 import { SharedModule } from "./shared/shared.module";
 import { AppComponent } from "./app.component";
-import { DemoComponent } from "./components/demo/demo.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    SharedModule
-  ],
-  declarations: [AppComponent, DemoComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MaterialModule } from "./material.module";
@@ -10,9 +11,10 @@ import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    CommonModule,
+    HttpClientModule,
     MaterialModule
   ],
   exports: [MaterialModule, NgxMaskDirective, ToolbarComponent],
